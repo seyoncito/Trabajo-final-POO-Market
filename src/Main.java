3f689974dd3731c4e8ffc145d28727c80abb4e27
Cesar Huamani <115020164+seyoncito@users.noreply.github.com>
2024-12-07 14:08:31 -0500

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,19 @@
+import Controller.LoginController;
+
+import javax.swing.*;
+import Model.UserModel;
+import Vista.LoginForm;
+
 public class Main {
     public static void main(String[] args) {
-
-        LoginPanel Home=new LoginPanel();
-        //Cliente Home =new Cliente();
-        Home.setVisible(true);
-
-        /*
-        JFrame frame = new JFrame("Sistema de Facturación");
-        frame.setSize(300, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout(2, 1));
-
-        JButton btnCreaCliente = new JButton("Crear Cliente");
-        JButton btnNuevaFactura = new JButton("Nueva Factura");
-
-        btnCreaCliente.addActionListener(e -> new RegistrCliente().setVisible(true));
-        btnNuevaFactura.addActionListener(e -> new FacturaNueva().setVisible(true));
-
-        frame.add(btnCreaCliente);
-        frame.add(btnNuevaFactura);
-
-        frame.setVisible(true);
-
-
-         */
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                UserModel model = new UserModel("admin", "admin");
+                LoginForm view = new LoginForm();
+                new LoginController(model, view);
+                view.setVisible(true);
+            }
+        });
     }
 }
